055d309c0e77f78f33648c50294789e47854b713
Victor Lourenço <vlourenco.m@gmail.com>
2021-02-04 22:12:27 -0300

    Started pokedex page

---
+++ src/pages/Pokedex.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const Pokedex: React.FC = () => {
+  return(
+    <SafeAreaView>
+      <Text>Pokedex</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Pokedex;
